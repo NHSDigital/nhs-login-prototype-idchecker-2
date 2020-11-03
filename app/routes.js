09196b47852104b13af8395c65309bb7b91e8ea5
Lydia Teebay <lydia@hippodigital.co.uk>
2020-11-03 11:59:10 +0000

    updating version e to be aligned with live

--- app/routes.js
+++ app/routes.js
@@ -21,7 +21,7 @@ router.post("/", function (req, res) {
   // pull in JSON data file
   delete req.session.data['idv']
   let idvFile = 'verification-requests.json'
-  let path = 'app/data/g/'
+  let path = 'app/data/a/'
   req.session.data['idv'] = loadJSONFromFile(idvFile, path)
 
   prototype.version = req.session.data.version
